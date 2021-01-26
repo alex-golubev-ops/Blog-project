8e5431d2cd2558fc7e99118d2b7f44b4081d3670
AlexGolubev <golubevaleksej7@gmail.com>
2021-01-27 01:41:26 +0300

    Update User

--- src/main/java/com/leverx/blog/entity/User.java
+++ src/main/java/com/leverx/blog/entity/User.java
@@ -63,7 +63,7 @@ public class User implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return isActive();
     }
 
     public Integer getId() {
