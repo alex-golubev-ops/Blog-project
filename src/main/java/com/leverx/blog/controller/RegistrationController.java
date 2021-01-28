12498a10cc6cebccb0504151ac812b3cc95b5616
AlexGolubev <golubevaleksej7@gmail.com>
2021-01-29 01:24:07 +0300

    Update RegistrationController

--- src/main/java/com/leverx/blog/controller/RegistrationController.java
+++ src/main/java/com/leverx/blog/controller/RegistrationController.java
@@ -1,25 +1,25 @@
 package com.leverx.blog.controller;
 
-import com.leverx.blog.entity.Role;
 import com.leverx.blog.entity.User;
 import com.leverx.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Collections;
 
 @Controller
 public class RegistrationController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/registration")
     public String registration() {
-
         return "registration";
     }
 
@@ -27,7 +27,19 @@ public class RegistrationController {
     public String addUser(User user, Model model) {
         if (!userService.save(user)) {
             model.addAttribute("emailmessage", "email is exist");
+            return "registration";
         }
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(@PathVariable String code, Model model) {
+        boolean isActivate = userService.activate(code);
+        if(isActivate){
+            model.addAttribute("message","User is activate");
+        }else {
+            model.addAttribute("message","User is not activate");
+        }
+        return "login";
     }
 }
