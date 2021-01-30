621ba7a403ea6c4fc1f8b2bf305b673168acdedb
AlexGolubev <golubevaleksej7@gmail.com>
2021-01-30 19:33:17 +0300

    Create java convention

--- src/main/java/com/leverx/blog/repository/CodeRepository.java
+++ src/main/java/com/leverx/blog/repository/CodeRepository.java
@@ -1,13 +1,9 @@
 package com.leverx.blog.repository;
 
-import org.springframework.data.keyvalue.repository.KeyValueRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-
 public interface CodeRepository {
-  void save(String code, String email);
-  String findByCode(String code);
-  void delete(String code);
+    void save(String code, String email);
+
+    String findByCode(String code);
+
+    void delete(String code);
 }
