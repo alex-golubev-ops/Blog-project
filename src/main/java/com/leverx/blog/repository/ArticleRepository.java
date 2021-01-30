e8a6d85904d880ce790cf079c49aa0a9489b8a80
AlexGolubev <golubevaleksej7@gmail.com>
2021-01-30 19:32:00 +0300

    Update ArticleRepository

--- src/main/java/com/leverx/blog/repository/ArticleRepository.java
+++ src/main/java/com/leverx/blog/repository/ArticleRepository.java
@@ -1,14 +1,15 @@
 package com.leverx.blog.repository;
 
 import com.leverx.blog.entity.Article;
-import com.leverx.blog.entity.User;
+import com.leverx.blog.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article,Integer> {
    Optional<Article> findById(Integer id);
+
+   List<Article> findByTags(Tag tag);
 }
