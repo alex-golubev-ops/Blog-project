3837b505e0ef55372229812ec6993cba1bb7b6e1
AlexGolubev <golubevaleksej7@gmail.com>
2021-01-30 20:17:10 +0300

    Update ArticleService. Create delegirate method of delete

--- src/main/java/com/leverx/blog/service/ArticleService.java
+++ src/main/java/com/leverx/blog/service/ArticleService.java
@@ -2,31 +2,35 @@ package com.leverx.blog.service;
 
 import com.leverx.blog.entity.Article;
 import com.leverx.blog.entity.Status;
-import com.leverx.blog.entity.User;
 import com.leverx.blog.repository.ArticleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class ArticleService {
-    @Autowired
-    private ArticleRepository articleRepository;
+    private final ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     public void save(Article article) {
         articleRepository.save(article);
     }
-    public List<Article> findList(boolean draftAndPublic){
-        if(draftAndPublic){
+
+    public List<Article> findList(Status status){
+        if(status==Status.ALL){
             return articleRepository.findAll();
         }
         else {
             return articleRepository.findAll().stream().filter(e->e.getStatus().equals(Status.PUBLIC))
                     .collect(Collectors.toList());
         }
+    }
+
+    public void delete(Article article){
+        articleRepository.delete(article);
     }
 
 
