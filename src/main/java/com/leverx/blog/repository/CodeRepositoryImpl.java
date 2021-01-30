cbc1d42942e54799b8031a088ee5b69bbdc6f49f
AlexGolubev <golubevaleksej7@gmail.com>
2021-01-30 19:33:55 +0300

    Create java convention

--- src/main/java/com/leverx/blog/repository/CodeRepositoryImpl.java
+++ src/main/java/com/leverx/blog/repository/CodeRepositoryImpl.java
@@ -1,22 +1,20 @@
 package com.leverx.blog.repository;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @Repository
 public class CodeRepositoryImpl implements CodeRepository {
+
     private RedisTemplate<String, String> redisTemplate;
 
     private final HashOperations hashOperations;
 
     public CodeRepositoryImpl(RedisTemplate<String, String> redisTemplate) {
         this.redisTemplate = redisTemplate;
-        redisTemplate.expire("CODS",1, TimeUnit.DAYS);
+        redisTemplate.expire("CODS", 1, TimeUnit.DAYS);
         hashOperations = redisTemplate.opsForHash();
     }
 
@@ -29,13 +27,13 @@ public class CodeRepositoryImpl implements CodeRepository {
 
     @Override
     public String findByCode(String code) {
-        return (String) hashOperations.get("CODS",code);
+        return (String) hashOperations.get("CODS", code);
 
     }
 
     @Override
     public void delete(String code) {
-        hashOperations.delete("CODS",code);
+        hashOperations.delete("CODS", code);
     }
 
 }
